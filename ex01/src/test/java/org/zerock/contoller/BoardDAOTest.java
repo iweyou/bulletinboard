7764d6f6947f2171ba0ef10379044d86944e4613
USER <USER@USER-PC>
2018-04-13 17:31:41 +0900

    board

---
+++ ex01/src/test/java/org/zerock/contoller/BoardDAOTest.java
@@ -0,0 +1,64 @@
+package org.zerock.contoller;
+
+
+import java.util.Objects;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import org.zerock.domain.BoardVO;
+
+import org.zerock.persistence.BoardDAO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/**/root-context.xml" })
+public class BoardDAOTest {
+
+  @Inject
+  private BoardDAO dao;
+
+  private static Logger logger = LoggerFactory.getLogger(BoardDAOTest.class);
+
+  @Test
+  public void testCreate() throws Exception {
+
+    BoardVO board = new BoardVO();
+    board.setTitle("새로운 글을 넣습니다. ");
+    board.setContent("새로운 글을 넣습니다. ");
+    board.setWriter("user00");
+    dao.create(board);
+  }
+
+  @Test
+  public void testRead() throws Exception {
+	Objects.requireNonNull(dao, "dao: null"); 
+	Objects.requireNonNull(dao.read(2), "dao.read(1): null");
+	
+    logger.info(dao.read(2).toString());
+  }
+
+  @Test
+  public void testUpdate() throws Exception {
+
+    BoardVO board = new BoardVO();
+    board.setBno(1);
+    board.setTitle("수정된 글입니다.");
+    board.setContent("수정 테스트 ");
+    dao.update(board);
+  }
+
+  @Test
+  public void testDelete() throws Exception {
+
+    dao.delete(1);
+  }
+
+  
+
+}
